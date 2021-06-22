aefbfefc7ed3891ab4d06baf66c4394ccfaf7d4d
Nikita Entin <entin@vbankcenter.ru>
2021-06-22 21:50:01 +0300

    bug fix

---
+++ MoviesCollection/API/MoviesViewModel.swift
@@ -0,0 +1,44 @@
+//
+//  MoviesViewModel.swift
+//  MoviesCollection
+//
+//  Created by Nikita Entin on 22.06.2021.
+//
+
+import Foundation
+
+struct MoviesViewModel: Codable {
+    let page: Int
+    let results: [Result]
+    let total_pages, total_results: Int
+
+}
+
+// MARK: - Result
+struct Result: Codable {
+    let adult: Bool
+    let backdropPath: String?
+    let genreIDS: [Int]
+    let id: Int
+    let originalTitle, overview: String
+    let popularity: Double
+    let posterPath, releaseDate, title: String?
+    let video: Bool
+    let voteAverage: Double
+    let voteCount: Int
+
+private enum CodingKeys: String, CodingKey {
+        case adult
+        case backdropPath = "backdrop_path"
+        case genreIDS = "genre_ids"
+        case id
+        case originalTitle = "original_title"
+        case overview, popularity
+        case posterPath = "poster_path"
+        case releaseDate = "release_date"
+        case title, video
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+    }
+
+}
